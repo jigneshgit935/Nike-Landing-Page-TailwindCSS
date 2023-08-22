c8520dbedecdeb5276971270f802a25eb4b4a1fd
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-23 03:37:47 +0530

    Navbar-created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Nav from './components/Nav';
 import {
   CustomerReviews,
   Footer,
@@ -11,6 +12,7 @@ import {
 
 const App = () => (
   <main className="relative">
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
